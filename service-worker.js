bb0a0cf9aefdb79bf3d030629d6b234a97ae3e4f
victortransit87 <victortransit87@gmail.com>
2025-09-17 09:11:11 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", event =
+  console.log("Service Worker instalado"); 
+}); 
+ 
+self.addEventListener("fetch", event =
+  // Aquí podrías cachear recursos si quisieras offline 
+}); 
